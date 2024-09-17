70d05244801681d4b68906ba8a26e167ced968e6
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-09-17 09:25:16 +0300

    korjattu navin linkitys

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-// import Home from './components/Home';
 import Statistics from './components/Statistics';
 import Auth from './components/Auth';
 import AddTransaction from './components/AddTransaction';
